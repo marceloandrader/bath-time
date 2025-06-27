b25d63c0b6bb838749c1caae9f64a3bc349b6b8a
Marcelo Andrade R. <marcelo.andrade.r@gmail.com>
2025-06-27 11:32:13 -0500

    static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
+      fallback: '404.html',
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+    }
 	}
 };
 
